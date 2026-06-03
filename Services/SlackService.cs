4b41df0f429aa31598a2874606991890fcb2779d
Popa, Diana-Bogdana <diana.popa@marsh.com>
2026-06-03 12:37:53 +0300

    temp hardcore webhook

--- Services/SlackService.cs
+++ Services/SlackService.cs
@@ -7,9 +7,10 @@ public class SlackService
 
     public SlackService(IConfiguration config, HttpClient http)
     {
-        _webhookUrl = Environment.GetEnvironmentVariable("SLACK_WEBHOOK_URL")
-        ?? config["Slack:WebhookUrl"];
-        _http = http;
+        _webhookUrl = "PASTE_YOUR_FULL_WEBHOOK_URL_HERE";
+        //_webhookUrl = Environment.GetEnvironmentVariable("SLACK_WEBHOOK_URL")
+        //?? config["Slack:WebhookUrl"];
+        //_http = http;
     }
 
     public async Task NotifyHolidayAdded(string person, string date)
